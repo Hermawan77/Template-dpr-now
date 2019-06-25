da30c61563560629d306f5611cf6482ec99643e3
aannisagita <annisagita10@gmail.com>
2019-06-25 08:40:58 +0700

    tambahan tampilan input

---
+++ app/src/main/java/com/example/template_dpr_now/FAB.java
@@ -0,0 +1,13 @@
+package com.example.template_dpr_now;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ViewFlipper;
+
+public class FAB extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab);
+    }
+}
