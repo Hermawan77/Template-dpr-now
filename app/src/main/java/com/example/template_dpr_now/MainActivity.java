89961b98744fabfbcce16531786935ff6cbb967f
ApaKabahrul <bahrul.faizi@gmail.com>
2019-06-26 08:24:33 +0700

    Merge branch 'master' of E:\SMT_7\KP with conflicts.

--- app/src/main/java/com/example/template_dpr_now/MainActivity.java
+++ app/src/main/java/com/example/template_dpr_now/MainActivity.java
@@ -1,42 +1,47 @@
 package com.example.template_dpr_now;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
-import android.renderscript.ScriptGroup;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String GOOGLE_ACCOUNT = "google_account";
-    Button crudbtn, homebtn;
+    public static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendNotification (View view){
+        /*NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setTicker("Testing")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My Notification")
+                .setContentText("Hello World!");
+
+        mBuilder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify (0, mBuilder.build());*/
 
 
-        crudbtn= findViewById(R.id.crudbtn);
-        homebtn = findViewById(R.id.homebtn);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.notification_icon);
+        builder.setAutoCancel(true);
+        builder.setContentTitle("Testing");
+        builder.setContentText("Coba notif");
+        builder.setSubText("Tap");
 
-        homebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MulaiActivity.class);
-                MainActivity.this.startActivity(intent);
-                MainActivity.this.finish();
-            }
-        });
-
-        crudbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Input.class);
-                MainActivity.this.startActivity(intent);
-                MainActivity.this.finish();
-            }
-        });
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
