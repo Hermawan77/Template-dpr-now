30ff0518005d5ba6baafaa3d5368f15c3b100b02
aannisagita <annisagita10@gmail.com>
2019-06-26 08:18:06 +0700

    tambahan tampilan input

--- app/src/main/java/com/example/template_dpr_now/MainActivity.java
+++ app/src/main/java/com/example/template_dpr_now/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.template_dpr_now;
 
+import android.content.Intent;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String GOOGLE_ACCOUNT = "google_account";
+    Button crudbtn, homebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        crudbtn= findViewById(R.id.crudbtn);
+        homebtn = findViewById(R.id.homebtn);
+
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MulaiActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+
+        crudbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Input.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/template_dpr_now/MulaiActivity.java
+++ app/src/main/java/com/example/template_dpr_now/MulaiActivity.java
@@ -2,12 +2,39 @@ package com.example.template_dpr_now;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MulaiActivity extends AppCompatActivity {
 
+    ViewFlipper v_flipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mulai);
+
+        int images[] = {R.drawable.foto1, R.drawable.foto2, R.drawable.foto3};
+
+        v_flipper =findViewById(R.id.v_flipper);
+
+//        for (int i = 0; i< images.length; i++){
+//            flipperImages(images(i));
+//        }
+
+        for (int image: images){
+            flipperImages(image);
+        }
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
