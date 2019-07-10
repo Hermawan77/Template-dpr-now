ba20fd2c272ea57b80d7d5282e6cba4c41089b71
ApaKabahrul <bahrul.faizi@gmail.com>
2019-07-10 08:32:59 +0700

    Fix Tema

--- app/src/main/java/com/example/template_dpr_now/Pengaturan.java
+++ app/src/main/java/com/example/template_dpr_now/Pengaturan.java
@@ -1,5 +1,6 @@
 package com.example.template_dpr_now;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,7 +27,7 @@ public class Pengaturan extends AppCompatActivity {
             public void onClick(View v) {
                 Theme.setTheme(getApplication(), 0);
                 recreate();
-                Toast.makeText(Pengaturan.this, "Restart aplikasi untuk perubahan", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Pengaturan.this, "Tema berhasil diubah", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -35,11 +36,15 @@ public class Pengaturan extends AppCompatActivity {
             public void onClick(View v) {
                 Theme.setTheme(getApplication(), 1);
                 recreate();
-                Toast.makeText(Pengaturan.this, "Restart aplikasi untuk perubahan", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Pengaturan.this, "Tema berhasil diubah", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
