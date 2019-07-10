67a1a50d82e64f40618d51d5886613fea2a491e3
Hermawan77 <37897937+Hermawan77@users.noreply.github.com>
2019-07-10 10:37:12 +0700

    Fixing Bug

--- app/src/main/java/com/example/template_dpr_now/SplashActivity.java
+++ app/src/main/java/com/example/template_dpr_now/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 2000;
@@ -16,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashActivity.this, ImageActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, Login.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
