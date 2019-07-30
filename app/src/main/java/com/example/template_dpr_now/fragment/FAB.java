9f28b432f36cd0741465fdcb2f9e954bba3ae99b
Hermawan77 <37897937+Hermawan77@users.noreply.github.com>
2019-07-30 11:54:48 +0700

    PDF & Image Picker di FAB

--- app/src/main/java/com/example/template_dpr_now/fragment/FAB.java
+++ app/src/main/java/com/example/template_dpr_now/fragment/FAB.java
@@ -1,9 +1,16 @@
 package com.example.template_dpr_now.fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +18,10 @@ import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.template_dpr_now.InputAspirasi;
 import com.example.template_dpr_now.Model.PostPutDelPengaduan;
 import com.example.template_dpr_now.R;
 import com.example.template_dpr_now.Rest.API_Client;
@@ -24,13 +34,18 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.app.Activity.RESULT_OK;
 import static android.support.constraint.Constraints.TAG;
 
 public class FAB extends Fragment {
+    private static final int PICK_IMAGE_REQUEST=1;
+
     AutoCompleteTextView edit_nama, edit_email;
     EditText edit_nomor, edit_aduan;
-    Button btsimpan;
+    Button btsimpan, pdf, image;
     API_Interface mApiInterface;
+    TextView txtpdf, txtimage;
+    Uri pdfUri, imageUri;
 
     @Nullable
     @Override
@@ -41,6 +56,11 @@ public class FAB extends Fragment {
         edit_email = view.findViewById(R.id.Email);
         edit_aduan = view.findViewById(R.id.aduan);
         btsimpan = view.findViewById(R.id.btnsimpan);
+        pdf = view.findViewById(R.id.pickpdf);
+        image = view.findViewById(R.id.pickimage);
+        txtpdf = view.findViewById(R.id.textpdf);
+        txtimage = view.findViewById(R.id.textimage);
+
         mApiInterface = API_Client.getClient().create(API_Interface.class); // meng-init yang ada di package REST
 
          btsimpan.setOnClickListener(new View.OnClickListener() {
@@ -72,7 +92,75 @@ public class FAB extends Fragment {
             }
         });
 
+        pdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+                    selectPdf();
+
+                }
+                else{
+                    ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},9);
+                }
+
+            }
+        });
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+                    selectImage();
+
+                }
+                else{
+                    ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                }
+
+            }
+        });
+
         return  view;
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==9&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectPdf();
+        }
+        else if(requestCode==1&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectImage();
+        }
+    }
+
+    private void selectPdf(){
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 86);
+    }
+
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==86 && resultCode==RESULT_OK && data!=null){
+            pdfUri=data.getData();
+            txtpdf.setText("A file is selected : "+ data.getData().getLastPathSegment());
+        }
+        else if(requestCode==1 && resultCode==RESULT_OK && data!=null){
+            imageUri=data.getData();
+            txtimage.setText("A file is selected : "+ data.getData().getLastPathSegment());
+        }
+        else{
+            Toast.makeText(getContext(), "Please select a file", Toast.LENGTH_SHORT).show();
+        }
     }
 }
