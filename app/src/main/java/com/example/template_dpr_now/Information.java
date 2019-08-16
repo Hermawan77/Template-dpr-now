fce4a288fb291936193159caa030b4406f9d49b6
ApaKabahrul <bahrul.faizi@gmail.com>
2019-08-16 08:21:18 +0700

    Menambahkan direct link ke github dari Activity Information

--- app/src/main/java/com/example/template_dpr_now/Information.java
+++ app/src/main/java/com/example/template_dpr_now/Information.java
@@ -1,13 +1,31 @@
 package com.example.template_dpr_now;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Information extends AppCompatActivity {
+
+    String URL = "https://github.com/aannisagita/Template_dpr_now";
+    TextView linkGithub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_layout);
+
+        linkGithub = findViewById(R.id.textlink);
+
+        linkGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(URL));
+                startActivity(intent);
+            }
+        });
     }
 }
