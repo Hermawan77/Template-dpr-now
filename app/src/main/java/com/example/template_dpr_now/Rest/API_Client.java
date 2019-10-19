40796094149885d260f273d16446494d744b5d8d
pevensey <yuliantopambudi45@gmail.com>
2019-10-19 16:49:19 +0700

    mengubah endpoint API fitur pengaduan

--- app/src/main/java/com/example/template_dpr_now/Rest/API_Client.java
+++ app/src/main/java/com/example/template_dpr_now/Rest/API_Client.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class API_Client {
-    public static final String BASE_URL = "https://api-dpr-now.cfapps.io";
+    public static final String BASE_URL = "https://api-template-dpr-now.cfapps.io";
 
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
